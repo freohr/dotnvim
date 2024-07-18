29675712561331c3d7375f35987abd98a640dbe3
Stephen Faure <faure.stephen@gmail.com>
2024-07-18 12:13:11 +0200

    Add config for auto build with latex lsp

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -28,13 +28,13 @@ return {
         ignore_filetypes = { -- disable format on save for specified filetypes
           -- "python",
           "javascript",
-          "vue"
+          "vue",
         },
       },
       disabled = { -- disable formatting capabilities for the listed language servers
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
         -- "lua_ls",
-        "ccls"
+        "ccls",
       },
       timeout_ms = 1000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
@@ -44,14 +44,40 @@ return {
     -- enable servers that you already have installed without mason
     servers = {
       -- "pyright"
-      "ccls"
+      "ccls",
     },
     -- customize language server configuration options passed to `lspconfig`
     ---@diagnostic disable: missing-fields
     config = {
       -- clangd = { capabilities = { offsetEncoding = "utf-8" } },
       bashls = {
-        filetypes = { 'sh', 'zsh' }
+        filetypes = { "sh", "zsh" },
+      },
+      texlab = {
+        settings = {
+          texlab = {
+            build = {
+              executable = "tectonic",
+              onSave = true,
+              args = {
+                "-X",
+                "compile",
+                "%f",
+                "--keep-logs",
+                "-Zsearch-path=.",
+                "-Zsearch-path=/home/stephen/.config/mdtopdf/rpg-module",
+                "-Zsearch-path=/home/stephen/workspace/projects/rpg_module",
+              },
+            },
+            forwardSearch = {
+              executable = "okular",
+              args = {
+                "--unique",
+                "file:%p#src%l%f",
+              },
+            },
+          },
+        },
       },
     },
     -- customize how language servers are attached
